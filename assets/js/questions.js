dbc90aa96ed25b7fc0fa5978d866105e4e4942e4
katkapsasky <kat_kapsasky@hotmail.com>
2022-08-02 15:45:59 +0000

    Create questions.js page

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+        question: 'Who was the chief god in greek mythology?',
+        answers: [
+            {text: 'Zeus', correct: true},
+            {text: 'Apollo', correct: false},
+            {text: 'Poseidon', correct: false},
+            {text: 'Hades', correct: false},
+        ]
+    },
+    {
+        question: 'question 2',
+        answers: [
+            {text: 'aaaa', correct: false},
+            {text: 'bbbb', correct: false},
+            {text: 'cccc', correct: true},
+            {text: 'dddd', correct: false},
+        ]
+    },
+    {
+        question: 'question 3',
+        answers: [
+            {text: 'aaaa', correct: false},
+            {text: 'bbbb', correct: false},
+            {text: 'cccc', correct: false},
+            {text: 'dddd', correct: true},
+        ]
+    }
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
 let startButton = document.getElementById('start-button');
+let nextButton = document.getElementById('next-button');
 let quizQuestions = document.getElementById('quiz-questions');
 quizQuestions.style.display = 'none'
 let questionElement = document.getElementById('questions');
 let answerButtonsElement = document.getElementById('answer-options');
+let optionButtons = answerButtonsElement.querySelectorAll("button");
 
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', ()=> {
+    // currentQuestionIndex + 1;
+    currentQuestionIndex++;
+    setNextQuestion();
+});
 
 function startGame() {
     startButton.style.visibility = 'hidden'
@@ -22,20 +29,17 @@ function setNextQuestion() {
 
 function showQuestion(question) {
     questionElement.innerText = question.question
+    question.answers.forEach(function(answer, outerIndex) { // loop over question answers from array
+        optionButtons.forEach(function(button, innerIndex) { // loop over html buttons
+            if (outerIndex === innerIndex) {
+                // console.log(button, answer);
+                button.innerText = answer.text;
+                button.dataset.correct = answer.correct;
+            }
+        });
+    });
 }
 
 function selectAnswer() {
 
 }
-
-const questions = [
-    {
-        question: 'Who was the chief god in greek mythology?',
-        answers: [
-            {text: 'Zeus', correct: true},
-            {text: 'Apollo', correct: false},
-            {text: 'Poseidon', correct: false},
-            {text: 'Hades', correct: false},
-        ]
-    }
-]
