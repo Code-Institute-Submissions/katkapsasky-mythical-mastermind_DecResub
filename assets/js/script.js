cb76b55d94b58c71e5efc89dcd3ef217edd0572c
katkapsasky <kat_kapsasky@hotmail.com>
2022-08-22 12:01:48 +0000

    Add multiple results depending on user score

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,18 @@
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
-      x.style.display = "none";
+        x.style.display = "none";
     } else {
-      x.style.display = "block";
+        x.style.display = "block";
     }
-  }
+}
 
+// variables for quiz functionality
 let startButton = document.getElementById('start')
 let quiz = document.getElementById('quiz')
 quiz.style.display = 'none'
 let answerOptions = document.querySelectorAll('.answer')
-let questionEl = document.getElementById('question')
+let question = document.getElementById('question')
 let a_text = document.getElementById('a_text')
 let b_text = document.getElementById('b_text')
 let c_text = document.getElementById('c_text')
@@ -28,28 +29,34 @@ let submitBtn = document.getElementById('submit')
 let currentQuiz = 0
 let score = 0
 
+/*
+ *load quiz when start button is clicked
+ *show quiz question and answer options
+ *count and show how many questions have been answered
+ */
 startButton.addEventListener('click', loadQuiz)
 
 function loadQuiz() {
-    
+
     quiz.style.display = 'block'
     startButton.style.display = 'none'
     deselectAnswers()
 
     let currentQuizData = questions[currentQuiz]
 
-    questionEl.innerText = currentQuizData.question
+    question.innerText = currentQuizData.question
     a_text.innerText = currentQuizData.a
     b_text.innerText = currentQuizData.b
     c_text.innerText = currentQuizData.c
     d_text.innerText = currentQuizData.d
 
-    questionCounter++ 
+    questionCounter++
     progressText.innerText = `Question ${questionCounter} of ${maxQuestions}`
     progressBarFull.style.width = `${(questionCounter/maxQuestions) * 100}%
     `
 }
 
+//check if user answer is correct
 function deselectAnswers() {
     answerOptions.forEach(answerOption => answerOption.checked = false)
 }
@@ -57,30 +64,43 @@ function deselectAnswers() {
 function getSelected() {
     let answer
     answerOptions.forEach(answerOption => {
-        if(answerOption.checked) {
+        if (answerOption.checked) {
             answer = answerOption.id
         }
     })
     return answer
 }
 
+/*
+ *moves to next question when submit button is clicked
+ *if there are questions remaining
+ *if all questions are submitted
+ *the user total score will be displayed
+ *along with a message
+ */
 submitBtn.addEventListener('click', () => {
     let answer = getSelected()
-    if(answer) {
-       if(answer === questions[currentQuiz].correct) {
-           score++
-       }
+    if (answer) {
+        if (answer === questions[currentQuiz].correct) {
+            score++
+        }
 
-       currentQuiz++
+        currentQuiz++
 
-       if(currentQuiz < questions.length) {
-           loadQuiz()
-       } else {
-           quiz.innerHTML = `
-           <h2>You answered ${score}/${questions.length} questions correctly</h2>
+        if (currentQuiz < questions.length) {
+            loadQuiz()
+        } else if (score > 7) {
+            quiz.innerHTML = `
+           <h2>You're a mythical mastermind! You answered ${score}/${questions.length} questions correctly</h2>
 
            <button onclick="location.reload()">Reload</button>
            `
-       }
+        } else {
+            quiz.innerHTML = `
+           <h2>Not quite a mythical mastermind... You answered ${score}/${questions.length} questions correctly</h2>
+
+           <button onclick="location.reload()">Reload</button>
+           `
+        }
     }
 })
