057fb51eebd93f7228eaa231c22d2614f910fc22
katkapsasky <kat_kapsasky@hotmail.com>
2022-08-23 10:00:25 +0000

    Add missing semicolons to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,66 +9,66 @@ function myFunction() {
 }
 
 // variables for quiz functionality
-let startButton = document.getElementById('start')
-let quiz = document.getElementById('quiz')
-quiz.style.display = 'none'
-let answerOptions = document.querySelectorAll('.answer')
-let question = document.getElementById('question')
-let a_text = document.getElementById('a_text')
-let b_text = document.getElementById('b_text')
-let c_text = document.getElementById('c_text')
-let d_text = document.getElementById('d_text')
+let startButton = document.getElementById('start');
+let quiz = document.getElementById('quiz');
+quiz.style.display = 'none';
+let answerOptions = document.querySelectorAll('.answer');
+let question = document.getElementById('question');
+let a_text = document.getElementById('a_text');
+let b_text = document.getElementById('b_text');
+let c_text = document.getElementById('c_text');
+let d_text = document.getElementById('d_text');
 
 let progressText = document.getElementById('progress-text');
 let progressBarFull = document.getElementById('progress-bar-full');
 let questionCounter = 0;
 let maxQuestions = 10;
 
-let submitBtn = document.getElementById('submit')
+let submitBtn = document.getElementById('submit');
 
-let currentQuiz = 0
-let score = 0
+let currentQuiz = 0;
+let score = 0;
 
 /*
  *load quiz when start button is clicked
  *show quiz question and answer options
  *count and show how many questions have been answered
  */
-startButton.addEventListener('click', loadQuiz)
+startButton.addEventListener('click', loadQuiz);
 
 function loadQuiz() {
 
-    quiz.style.display = 'block'
-    startButton.style.display = 'none'
-    deselectAnswers()
+    quiz.style.display = 'block';
+    startButton.style.display = 'none';
+    deselectAnswers();
 
-    let currentQuizData = questions[currentQuiz]
+    let currentQuizData = questions[currentQuiz];
 
-    question.innerText = currentQuizData.question
-    a_text.innerText = currentQuizData.a
-    b_text.innerText = currentQuizData.b
-    c_text.innerText = currentQuizData.c
-    d_text.innerText = currentQuizData.d
+    question.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
 
-    questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${maxQuestions}`
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter} of ${maxQuestions}`;
     progressBarFull.style.width = `${(questionCounter/maxQuestions) * 100}%
-    `
+    `;
 }
 
 //check if user answer is correct
 function deselectAnswers() {
-    answerOptions.forEach(answerOption => answerOption.checked = false)
+    answerOptions.forEach(answerOption => answerOption.checked = false);
 }
 
 function getSelected() {
-    let answer
+    let answer;
     answerOptions.forEach(answerOption => {
         if (answerOption.checked) {
-            answer = answerOption.id
+            answer = answerOption.id;
         }
-    })
-    return answer
+    });
+    return answer;
 }
 
 /*
@@ -79,28 +79,28 @@ function getSelected() {
  *along with a message
  */
 submitBtn.addEventListener('click', () => {
-    let answer = getSelected()
+    let answer = getSelected();
     if (answer) {
         if (answer === questions[currentQuiz].correct) {
-            score++
+            score++;
         }
 
-        currentQuiz++
+        currentQuiz++;
 
         if (currentQuiz < questions.length) {
-            loadQuiz()
+            loadQuiz();
         } else if (score > 7) {
             quiz.innerHTML = `
            <h2>You're a mythical mastermind! You answered ${score}/${questions.length} questions correctly</h2>
 
            <button onclick="location.reload()">Reload</button>
-           `
+           `;
         } else {
             quiz.innerHTML = `
            <h2>Not quite a mythical mastermind... You answered ${score}/${questions.length} questions correctly</h2>
 
            <button onclick="location.reload()">Reload</button>
-           `
+           `;
         }
     }
-})
+});
